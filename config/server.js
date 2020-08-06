19d7422dbbba0f4a3b573e18f5c97ff38497ce41
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-08-05 23:56:38 -0300

    Rebase README

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '031e014c74ccff2413b544311a711ef1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9079d00e-48a8-41c1-857e-24be43c37242'
+};
